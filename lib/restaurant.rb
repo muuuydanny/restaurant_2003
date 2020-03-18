cb331a10b6aa0478f5a835f35052922b80b04959
Danny Ramos <danramos21@gmail.com>
2020-03-18 14:02:56 -0600

    Initial Commit

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,1 +1,22 @@
+class Restaurant
+  def initialize(opening_time, name)
+    @opening_time = opening_time
+    @name = name
+  end
 
+  def opening_time(opening_time)
+
+  end
+
+  def closing_time(close)
+    @closing_time = (close)
+  end
+
+  def dishes
+    @dishes = [] #I know what I did wrong here. Just not sure how else to do it.
+  end
+
+  attr_reader :opening_time
+  attr_reader :name
+
+end
